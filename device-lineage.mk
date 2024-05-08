7b88f8717932a02cd8253c6132381b71d3e633b7
Austin Young <austineyoung2000@gmail.com>
2024-05-08 13:26:26 -0400

    shusky: define battery capacity (shiba) & flag for PixelParts
    Change-Id: If0743fd177fd0863b9564ac7d09731cf9aecb665

--- device-lineage.mk
+++ device-lineage.mk
@@ -22,6 +22,9 @@ PRODUCT_PACKAGES += \
 # Kernel
 TARGET_PREBUILT_KERNEL := device/google/shusky-kernel/Image.lz4
 
+# PixelParts
+include packages/apps/PixelParts/device.mk
+
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
 
