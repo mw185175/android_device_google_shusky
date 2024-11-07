91947d4cb5e5570616003ef68d69112151355c42
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-11-06 19:19:25 -0500

    Initial device tree, first commit

---
+++ BoardConfigLineage.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+DEVICE_PATH := device/google/shusky
---
+++ orion_husky.mk
@@ -0,0 +1,103 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Lineage stuff.
+TARGET_DISABLE_EPPE := true
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/shusky/aosp_husky.mk)
+$(call inherit-product, device/google/zuma/lineage_common.mk)
+
+include device/google/shusky/husky/device-lineage.mk
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 8 Pro
+PRODUCT_NAME := orion_husky
+
+# UDFPS support 
+TARGET_HAS_UDFPS := true
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Blur
+TARGET_ENABLE_BLUR := true
+
+# BCR (Call recording)
+TARGET_PREBUILT_BCR := true
+
+# Lawnchair (Pixel launcher by default)
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
+# Exclude Aperture camera
+PRODUCT_NO_CAMERA := true
+
+# Google Camera App
+TARGET_PREBUILT_GOOGLE_CAMERA := true
+
+# Custom package installer
+TARGET_USE_CUSTOM_PACKAGE_INSTALLER := true
+
+# Live wallpapers
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+
+# Quick tap
+TARGET_SUPPORTS_QUICK_TAP  := true
+
+# Now Playing
+TARGET_SUPPORTS_NOW_PLAYING := true
+
+# Clear Calling
+TARGET_SUPPORTS_CLEAR_CALLING := true
+
+# Call Recording Support
+TARGET_SUPPORTS_CALL_RECORDING := true
+
+# Pixel Carrier Settings
+TARGET_INCLUDE_CARRIER_SETTINGS := true
+TARGET_INCLUDE_PIXEL_IMS := true
+TARGET_INCLUDE_PIXEL_EUICC := true
+TARGET_INCLUDE_CARRIER_SERVICES := true
+
+# A bunch of Pixel stuff
+USE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_DREAMLINER := true
+TARGET_SUPPORTS_GOOGLE_BATTERY := true
+TARGET_SUPPORTS_ADPATIVE_CHARGING := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_CAMERA := true
+TARGET_GOOGLE_CAMERA_LARGE_RAM := true
+TARGET_PIXEL_EXPERIENCE_2023 := true
+
+# GMS
+WITH_GMS := false
+
+# Orion OS Flags
+ORION_MAINTAINER="GamerBoy1234294"
+ORION_MAINTAINER_LINK := "https://t.me/GamerBoy1234294"
+ORION_BUILD_TYPE := UNOFFICIAL
+ORION_GAPPS := false
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2992
+TARGET_SCREEN_WIDTH := 1344
+TARGET_BOOT_ANIMATION_RES := 1440
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=husky \
+    PRIVATE_BUILD_DESC="husky-user 14 AP2A.240905.003 12231197 release-keys"
+
+BUILD_FINGERPRINT := google/husky/husky:14/AP2A.240905.003/12231197:user/release-keys
+
+$(call inherit-product, vendor/google/husky/husky-vendor.mk)
