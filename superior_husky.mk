e3b76135dda974333e230cb16891ae87072e335b
mw185175 <162651027+mw185175@users.noreply.github.com>
2024-11-08 20:51:46 -0700

    Update and rename orion_husky.mk to superior_husky.mk

--- superior_husky.mk
+++ superior_husky.mk
@@ -17,7 +17,7 @@ include device/google/shusky/husky/device-lineage.mk
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 8 Pro
-PRODUCT_NAME := orion_husky
+PRODUCT_NAME := superior_husky
 
 # UDFPS support 
 TARGET_HAS_UDFPS := true
@@ -78,13 +78,8 @@ TARGET_GOOGLE_CAMERA_LARGE_RAM := true
 TARGET_PIXEL_EXPERIENCE_2023 := true
 
 # GMS
-WITH_GMS := false
+WITH_GMS := true
 
-# Orion OS Flags
-ORION_MAINTAINER="GamerBoy1234294"
-ORION_MAINTAINER_LINK := "https://t.me/GamerBoy1234294"
-ORION_BUILD_TYPE := UNOFFICIAL
-ORION_GAPPS := false
 BUILD_GOOGLE_CONTACTS := true
 BUILD_GOOGLE_DIALER := true
 BUILD_GOOGLE_MESSAGE := true
